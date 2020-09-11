2dce47e4136d648d644e5605f38790df88ee663c
es9d <larastar721@gmail.com>
2020-09-12 08:38:50 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/cleanziy/delivery/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.cleanziy.delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }*/
+}
